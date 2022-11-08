0c1bcd5abc650bc9e2c5c655921eb60b5e1a65da
Matis <53236299+matis-io@users.noreply.github.com>
2022-11-08 12:05:32 +0100

    Add files via upload

---
+++ MdCR.py
@@ -0,0 +1,27 @@
+# Importing needed modules for phy
+
+import os
+
+# Conditions
+
+# Files that end with these exstensions will be moved and renamed
+Extension = (".m4a",".M4a",".M4A", ".mp3", ".Mp3", ".MP3")
+# RootDir is the current iTunes library music folder
+RootDir = r'/Users/LukeSkywalker/Music/iTunes/iTunes\ Media/Music'
+# NewDir is a new directory where you want to move your music
+NewDir = r'/Users/LukeSkywalker/Music/NewLibrary'
+
+# Renaming Files
+
+for root, dirs, files in os.walk((os.path.normpath(RootDir)), topdown=False):
+    for f in files:  # for any file with a name in the dir
+        if f.endswith(Extension):
+            ArtistDirectory, AlbumDirectory = (root.split(os.sep)[-2:])  # Get Artist and Album from Folder Names
+            RootPathAndName = os.path.join(root, f)  # Join Root Path and File Name
+            NewFileName = (ArtistDirectory + " - " + f)  # Append new File Name
+            NewPathAndName = os.path.join(NewDir, NewFileName)  # Append Path and File Name
+            os.rename(RootPathAndName, NewPathAndName)  # Rename Root Path and File Name with New Root Path and File 
+            # Name
+            print(RootPathAndName, NewPathAndName)  # Print All Touched Files
+
+print("Done")
---
+++ Readme.md
@@ -0,0 +1,16 @@
+# MdCR
+### Music deConsolidator
+
+MdCR is a simple Python script that de-consolidates iTunes/Music library. It moves all the music files into one folder and adds the artist name to the filename.
+
+### Warning! Attention!
+**Please create a backup of your music before running the script. The script removes the files from origin and is irreversible.**
+
+### How to use
+Before running the script please edit RootDir and NewDir parameters to your liking.
+
+### Donation
+If you found this script useful please consider donating by purchasing a song on my bandcamp. I get a donation, you get a song. Win-Win.
+
+### Description
+The script checkes the iTunes/Music library folder and grabs the 
